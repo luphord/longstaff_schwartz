8eec8ba087af0b7c40300badda189fb78ee742c1
luphord <luphord@protonmail.com>
2019-08-05 06:48:50 +0200

    add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,14 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = ['Click>=6.0',
+                'numpy>=1.14',
+                'scipy>=1.2',
+                'matplotlib>=3.0']
 
-setup_requirements = [ ]
+setup_requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="luphord",
