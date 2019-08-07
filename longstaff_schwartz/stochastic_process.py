28daa1b6e5e7bedf04dca6db55403c3a14a64a52
luphord <luphord@protonmail.com>
2019-08-07 17:38:04 +0200

    move GeometricBrownianMotion to Python module

--- longstaff_schwartz/stochastic_process.py
+++ longstaff_schwartz/stochastic_process.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from scipy.stats.distributions import norm, rv_frozen
+from scipy.stats.distributions import norm, lognorm, rv_frozen
 
 
 class BrownianMotion:
@@ -23,3 +23,26 @@ class BrownianMotion:
 
     def distribution(self, t: float) -> rv_frozen:
         return norm(self.mu * t, self.sigma * np.sqrt(t))
+
+
+class GeometricBrownianMotion:
+    '''Geometric Brownian Motion.(with optional drift).'''
+    def __init__(self, mu: float=0.0, sigma: float=1.0):
+        self.mu = mu
+        self.sigma = sigma
+
+    def simulate(self, t: np.array, n: int, rnd: np.random.RandomState) \
+            -> np.array:
+        assert t.ndim == 1, 'One dimensional time vector required'
+        assert t.size > 0, 'At least one time point is required'
+        dt = np.concatenate((t[0:1], np.diff(t)))
+        assert (dt >= 0).all(), 'Increasing time vector required'
+        # transposed simulation for automatic broadcasting
+        dW = (rnd.normal(size=(t.size, 1000)).T * np.sqrt(dt)).T
+        W = np.cumsum(dW, axis=0)
+        return np.exp(self.sigma * W.T + (self.mu - self.sigma**2 / 2) * t).T
+
+    def distribution(self, t: float) -> rv_frozen:
+        mu_t = (self.mu - self.sigma**2/2) * t
+        sigma_t = self.sigma * np.sqrt(t)
+        return lognorm(scale=np.exp(mu_t), s=sigma_t)
