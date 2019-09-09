aa51b4feff151450cf25e54eed00736714d7407d
luphord <luphord@protonmail.com>
2019-09-09 06:42:09 +0200

    added test vector

--- tests/test_longstaff_schwartz.py
+++ tests/test_longstaff_schwartz.py
@@ -7,7 +7,20 @@
 import unittest
 from click.testing import CliRunner
 
+import numpy as np
+
 from longstaff_schwartz import cli
+
+
+test_X = np.array([
+    [1.00, 1.09, 1.08, 1.34],
+    [1.00, 1.16, 1.26, 1.54],
+    [1.00, 1.22, 1.07, 1.03],
+    [1.00, 0.93, 0.97, 0.92],
+    [1.00, 1.11, 1.56, 1.52],
+    [1.00, 0.76, 0.77, 0.90],
+    [1.00, 0.92, 0.84, 1.01],
+    [1.00, 0.88, 1.22, 1.34]]).T
 
 
 class TestLongstaff_schwartz(unittest.TestCase):
