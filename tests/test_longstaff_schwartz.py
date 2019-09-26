957c8beb6852d8b7f1db7a39e3813d5fc8f541bd
luphord <luphord@protonmail.com>
2019-09-26 06:59:35 +0200

    simplify names

--- tests/test_longstaff_schwartz.py
+++ tests/test_longstaff_schwartz.py
@@ -14,7 +14,10 @@ from longstaff_schwartz.algorithm import \
     longstaff_schwartz_american_option_quadratic
 
 
-test_X = np.array([
+# Test values from chapter 1, Numerical Example
+# of originial Longstaff-Schwartz paper
+
+X = np.array([
     [1.00, 1.09, 1.08, 1.34],
     [1.00, 1.16, 1.26, 1.54],
     [1.00, 1.22, 1.07, 1.03],
@@ -24,11 +27,11 @@ test_X = np.array([
     [1.00, 0.92, 0.84, 1.01],
     [1.00, 0.88, 1.22, 1.34]]).T
 
-test_t = np.array([0, 1, 2, 3])
+t = np.array([0, 1, 2, 3])
 
-test_r = 0.06
+r = 0.06
 
-test_strike = 1.1
+strike = 1.1
 
 
 class TestLongstaff_schwartz(unittest.TestCase):
@@ -44,11 +47,9 @@ class TestLongstaff_schwartz(unittest.TestCase):
         assert '--help  Show this message and exit.' in help_result.output
 
     def test_longstaff_schwartz_paper_example(self):
-        value = longstaff_schwartz_american_option_quadratic(test_X, test_t,
-                                                             test_r,
-                                                             test_strike)
+        '''Test against example value of original Longstaff-Schwartz paper'''
+        value = longstaff_schwartz_american_option_quadratic(X, t, r, strike)
         self.assertEqual(0.1144, np.round(value, 4))
-        df = np.exp(-test_r * (test_t[-1] - test_t[0]))
-        european_value = np.maximum(test_strike - test_X[-1, :], 0.0).mean() \
-            * df
+        df = np.exp(-r * (t[-1] - t[0]))
+        european_value = np.maximum(strike - X[-1, :], 0.0).mean() * df
         self.assertEqual(0.0564, np.round(european_value, 4))
