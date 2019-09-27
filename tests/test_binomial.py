ebeec3a9182be60c63e978c738591b26522dfd24
luphord <luphord@protonmail.com>
2019-09-27 20:15:23 +0200

    test american call = european call

--- tests/test_binomial.py
+++ tests/test_binomial.py
@@ -85,3 +85,15 @@ class TestBinomial(unittest.TestCase):
                         actual.append(european_call_price(mdl, strike) -
                                       european_put_price(mdl, strike))
         self.assertTrue(np.allclose(expected, actual))
+
+    def test_american_european_call_equality(self):
+        european = []
+        american = []
+        for sigma in [0.05, 0.1, 0.2]:
+            for r in [-0.02, 0.0, 0.01, 0.1]:
+                for S0 in [50, 100, 5000]:
+                    mdl = create_binomial_model(0.1, 0.02, 100, 1, n=100)
+                    for strike in [80, 90, 100, 110, 120]:
+                        european.append(european_call_price(mdl, strike))
+                        american.append(american_call_price(mdl, strike))
+        self.assertTrue(np.allclose(european, american))
