89ddc4c7ed94e5d085b7aa3795fac5ae8c29c396
luphord <luphord@protonmail.com>
2022-11-10 21:08:42 +0100

    drop support for Python 3.7

--- HISTORY.rst
+++ HISTORY.rst
@@ -2,7 +2,13 @@
 History
 =======
 
+0.2.0 (not yet)
+---------------
+
+* Drop support for Python 3.7
+
 0.1.1 (2020-12-01)
+------------------
 
 * Support Python 3.8
 * Migrate to travis-ci.com
--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
     description="A Python implementation of the Longstaff-Schwartz linear regression algorithm for the evaluation of call rights and American options.",
